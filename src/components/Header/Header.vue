6ca2e76c7e46e08f24ce35f9dab88ebdcdd10938
Nick <melancholic8078@gmail.com>
2025-09-12 15:55:16 +0300

    modal toogle

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -1,10 +1,21 @@
-<script>
+<script setup lang="ts">
+import HeaderModal from '../HeaderModal/HeaderModal.vue'
+import { ref } from 'vue';
+
+const modalOpen = ref(false)
+
+const modalSwitchToogle = ()=>{
+  modalOpen.value = !modalOpen.value
+}
+
 </script>
+
+
 <template>
     <header class="header-section container" id="home">
       <div class="div-img">
         <img src="../../assets/images/bose-logo.png" alt="bose-logo" class="bose-logo" />
-        <button type="button" class="burger-menu-button">
+        <button type="button" class="burger-menu-button" @click="modalSwitchToogle">
           <a href="#menu">
             <svg class="burger-menu burger-icon-320">
               <use href="../../assets/images/svgpic.svg#icon-Icon-Burger-menu"></use>
@@ -22,84 +33,13 @@
           Your music shines.
         </h1>
       </div>
-        <div class="menu is-open" id="menu">
-      <div class="div-img-2">
-        <img src="../../assets/images/bose-logo.png" alt="bose-logo" class="img-logo" />
-        <button type="button" class="burger-menu-button">
-          <a href="#home">
-            <svg class="burger-menu icon-close">
-              <use href="../../assets/images/svgpic.svg#icon-Icon-Close"></use>
-            </svg>
-          </a>
-        </button>
-      </div>
-
-      <ul class="menu-ul">
-        <li class="menu-link">
-          <a href="#home" class="link-nav">Home</a>
-        </li>
-        <li class="menu-link">
-          <a href="#recommended" class="link-nav">recommended</a>
-        </li>
-        <li class="menu-link">
-          <a href="#products" class="link-nav">Categories</a>
-        </li>
-        <li class="menu-link">
-          <a href="#benefits" class="link-nav">How to buy</a>
-        </li>
-      </ul>
-    </div>
+    <HeaderModal v-if="modalOpen" :isOpen="modalOpen" @close ="modalOpen = false"/>
     </header>
 </template>
 
+
+
 <style scoped>
-.is-open {
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  padding: 35px 18px;
-  background-color: #e8eff9;
-  position: fixed;
-  top: 0;
-  right: 0;
-  left: 0;
-  transform: translateX(-100%);
-  transition: transform 500ms;
-  height: 100vh;
-  box-sizing: border-box;
-}
-.is-open:target {
-  transform: translateX(0);
-}
-.div-img-2 {
-  display: flex;
-  justify-content: space-between;
-}
-.img-logo {
-  width: 127px;
-  height: 16px;
-}
-.link-nav {
-  color: #292929;
-
-  font-family: Inter;
-  font-size: 22px;
-  font-style: normal;
-  font-weight: 700;
-  line-height: 1em;
-  letter-spacing: 0.09em;
-  text-transform: uppercase;
-  text-decoration: none;
-}
-.menu-ul {
-  display: flex;
-  flex-direction: column;
-  gap: 25px;
-  margin: 0;
-  padding-bottom: 90px;
-
-  list-style-type: none;
-}
 .burger-icon-320 {
   fill: white;
 }
@@ -107,7 +47,6 @@
 .header-section {
   width: 100%;
   height: 500px;
-
   padding: 32px 16px;
   box-sizing: border-box;
   background-image: url("../../assets/images/background-header.png");
@@ -157,30 +96,8 @@
   .menu-ul {
     gap: 20px;
   }
-  .rec-section {
-    padding: 0 20px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
-  .product-li {
-    display: flex;
-    justify-content: center;
-    flex-direction: column;
-    width: 400px;
-  }
-  .product-ul :nth-child(-n + 3) {
-    display: flex;
-    align-items: center;
-  }
 }
 @media screen and (min-width: 1200px) {
-  .product-ul {
-    flex-direction: row;
-  }
-  .product-li {
-    width: 300px;
-  }
   img {
     width: 100%;
   }
@@ -220,24 +137,6 @@
   ul {
     list-style-type: none;
     padding: 0;
-  }
-  .is-open {
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    padding: 64px 120px;
-    background-color: #e8eff9;
-    position: fixed;
-    top: 0;
-    right: 0;
-    left: 0;
-    transform: translateX(-100%);
-    transition: transform 500ms;
-    height: 100vh;
-    box-sizing: border-box;
-  }
-  .is-open:target {
-    transform: translateX(0);
   }
 
   .div-img-2 {
---
+++ src/components/HeaderModal/HeaderModal.vue
@@ -0,0 +1,155 @@
+<script setup lang="ts">
+import { defineProps,computed } from 'vue';
+
+const props = defineProps({
+    isOpen: Boolean,
+})
+const emit = defineEmits(['close'])
+const modalClass = computed(()=>{
+    return props.isOpen ?  'is-open' : ''
+})
+</script>
+
+
+<template>
+         <div class="menu" id="menu"  :class="modalClass">
+      <div class="div-img-2">
+        <img src="../../assets/images/bose-logo.png" alt="bose-logo" class="img-logo" />
+        <button type="button" class="burger-menu-button" @click="$emit('close')">
+          <a href="#home">
+            <svg class="burger-menu icon-close">
+              <use href="../../assets/images/svgpic.svg#icon-Icon-Close"></use>
+            </svg>
+          </a>
+        </button>
+      </div>
+      <div>
+      <ul class="menu-ul">
+        <li class="menu-link">
+          <a href="#home" class="link-nav">Home</a>
+        </li>
+        <li class="menu-link">
+          <a href="#recommended" class="link-nav">recommended</a>
+        </li>
+        <li class="menu-link">
+          <a href="#products" class="link-nav">Categories</a>
+        </li>
+        <li class="menu-link">
+          <a href="#benefits" class="link-nav">How to buy</a>
+        </li>
+      </ul>
+      </div>
+    </div>
+</template>
+<style scoped>
+
+.div-img-2 {
+  display: flex;
+  justify-content: space-between;
+}
+.burger-menu-button {
+  background: transparent;
+  border: none;
+}
+.img-logo {
+  width: 127px;
+  height: 16px;
+}
+.menu-ul {
+  display: flex;
+  flex-direction: column;
+  gap: 25px;
+  margin: 0;
+  padding-bottom: 90px;
+
+  list-style-type: none;
+}
+.link-nav {
+  color: #292929;
+
+  font-family: Inter;
+  font-size: 22px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 1em;
+  letter-spacing: 0.09em;
+  text-transform: uppercase;
+  text-decoration: none;
+}
+.burger-menu {
+  width: 24px;
+  height: 24px;
+}
+
+@media screen and (min-width: 472px) {
+  /* =================Header menu settings================== */
+  .burger-menu {
+    fill: black;
+  }
+  .menu-ul {
+    gap: 20px;
+  }
+}
+@media screen and (min-width: 1200px) {
+  img {
+    width: 100%;
+  }
+}
+@media screen and (min-width: 1260px) {
+  /* =================Header================== */
+  .burger-menu {
+    width: 24px;
+    height: 24px;
+  }
+  a {
+    text-decoration: none;
+  }
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+
+  .div-img-2 {
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 64px;
+  }
+  .link-nav {
+    color: #292929;
+
+    font-family: Inter;
+    font-size: 22px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: 1em;
+    letter-spacing: 0.09em;
+    text-transform: uppercase;
+  }
+  .menu-ul {
+    display: flex;
+    flex-direction: column;
+    gap: 32px;
+    margin: 0;
+    padding-left: 104px;
+  }
+  .is-open {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    /* padding: 64px 120px; */
+    padding: 32px 16px;
+    background-color: #e8eff9;
+    position: fixed;
+    top: 0;
+    right: 0;
+    left: 0;
+    transform: translateX(-100%);
+    transition: transform 500ms;
+    height: 100vh;
+    box-sizing: border-box;
+  }
+  .is-open:target {
+    transform: translateX(0);
+  }
+}
+</style>
